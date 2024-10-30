65238f21127abd502943302e7038f63063acf6b7
Desmond Roberts <desmondr1@aol.com>
2024-10-29 22:31:06 -0700

    project structure changed

---
+++ src/paths.py
@@ -0,0 +1,22 @@
+"""
+Contains constants that show provide paths to useful locations in the project.
+"""
+
+from pathlib import Path
+
+
+ROOT_DIR = Path(__file__).resolve().parent.parent
+
+# Assets
+ASSETS_DIR = ROOT_DIR / "assets"
+SPRITESHEETS_DIR = ASSETS_DIR / "spritesheets"
+IMAGES_DIR = ASSETS_DIR / "images"
+SOUNDS_DIR = ASSETS_DIR / "sounds"
+FONTS_DIR = ASSETS_DIR / "fonts"
+MUSIC_DIR = ASSETS_DIR / "music"
+PARALLAX_DIR = ASSETS_DIR / "images" / "parallax_backgrounds"
+
+# Saving
+SAVE_DIR = ROOT_DIR / "save"
+SAVED_DATA_DIR = SAVE_DIR / "data"
+SAVE_DATA_TEMPLATES_DIR = SAVE_DIR / "data_templates"
